daf845c8b991066d95b265a652afb529ae3c1717
Maid <zhp92@outlook.com>
2022-06-14 23:37:50 +0800

    test: :test_tube: Test vue3-calendar-heatmap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
 import './App.css'
 import Home from './page/home';
+import { CalendarHeatmap } from 'vue3-calendar-heatmap';
 
 export default defineComponent({
   props: {
   },
 
   setup(props) {
+    const values = reactive([
+      {
+        'date': '2022-06-13T00:00:00Z',
+        'count': 635000
+      },
+      {
+        'date': '2022-06-14T00:00:00Z',
+        'count': 2442000
+      },
+    ]);
+    const orientation = ref("horizontal");
     return () => (
       <>
-        <Home/>
+        <Home />
+        <CalendarHeatmap
+          values={values}
+          endDate={new Date()}
+          style={{ 'max-width:': (orientation.value == 'vertical' ? '145px' : '675px') }}
+          vertical={orientation.value === 'vertical'}
+          noDataText="NOTHING"
+          round={2}
+        ></CalendarHeatmap>
       </>
     )
   }
