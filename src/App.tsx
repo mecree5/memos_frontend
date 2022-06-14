3797fefaf554c3c12e72c3a4dca90466acf20043
Maid <zhp92@outlook.com>
2022-06-14 22:27:56 +0800

    feat: :sparkles: Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld'
 import './App.css'
+import Home from './page/home';
 
 export default defineComponent({
   props: {
   },
 
   setup(props) {
-
     return () => (
       <>
-        <img alt="Vue logo" src="@/assets/logo.png" />
-        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+        <Home/>
       </>
     )
   }
