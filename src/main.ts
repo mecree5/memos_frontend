42639a2da8bf77fc75f6f994ce0bbf3c9780a0f2
Maid <zhp92@outlook.com>
2022-09-12 17:31:27 +0800

    feat: import VMdEditor

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App'
 
-createApp(App).mount('#app')
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+// highlightjs
+import hljs from 'highlight.js';
+import createAlignPlugin from '@kangc/v-md-editor/lib/plugins/align';
+
+
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+VMdEditor.use(githubTheme, {
+  Hljs: hljs,
+});
+VMdEditor.use(createAlignPlugin());
+
+VMdPreview.use(githubTheme, {
+  Hljs: hljs,
+});
+
+const app = createApp(App);
+app.use(VMdEditor);
+app.use(VMdPreview); 
+
+app.mount('#app')
---
+++ src/shims.d.ts
@@ -0,0 +1,5 @@
+declare module '@kangc/v-md-editor';
+declare module '@kangc/v-md-editor/lib/theme/github.js';
+declare module '@kangc/v-md-editor/lib/plugins/align';
+
+declare module '@kangc/v-md-editor/lib/preview'
