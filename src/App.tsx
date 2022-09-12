d8b44c8e85847f1ebc776a10afd1e7c3f77ad55b
Maid <zhp92@outlook.com>
2022-09-12 17:26:38 +0800

    refactor: add home pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
-import { defineComponent, reactive, ref } from 'vue';
+import { defineComponent } from 'vue';
 import './App.css'
 import Home from './pages/home';
-import { CalendarHeatmap } from './components/vue3-calendar-heatmap';
-import UserInfo from './components/userinfo';
 
 export default defineComponent({
   props: {
   },
-
   setup(props) {
-    const values = reactive([
-      {
-        'date': '2022-06-13T00:00:00Z',
-        'count': 635000
-      },
-      {
-        'date': '2022-06-14T00:00:00Z',
-        'count': 2442000
-      },
-    ]);
-    const orientation = ref("horizontal");
-    return () => (
-      <>
-        <div style="width:300px">
-          <UserInfo />
-          <CalendarHeatmap
-            values={values}
-            endDate={new Date()}
-            showDateNum={100}
-            rangeColor={['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39']}
-            vertical={orientation.value === 'vertical'}
-            noDataText="NOTHING"
-            round={2}
-          ></CalendarHeatmap>
-        </div>
-      </>
-    )
+
+    return () => <Home/>
   }
 })
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,27 @@
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import MDEditor, { MODE } from '/@/components/md-editor';
+import Sidebar from "/@/components/sidebar";
+import "./index.css";
 
 
 export default defineComponent({
-  setup(props) {
+  name: "Home",
+  setup() {
+    const text = ref('Any thounghts...');
+    let mode = ref(MODE.EDIT);
+
     return () => (
-      <> This is home page </>
-    )
+      <>
+        <div class="container mx-auto relative w-full min-h-screen flex flex-row justify-start sm:justify-center items-start;">
+          <Sidebar />
+          <main class="memos-wrapper">
+            <MDEditor
+              v-model={text.value}
+              v-model:mode={mode}
+            ></MDEditor>
+          </main>
+        </div>
+      </>
+    );
   }
 })
