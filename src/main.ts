eab400d8119f57fb2e88853c4c21feb0b619837c
Maid <zhp92@outlook.com>
2022-09-12 18:43:20 +0800

    feat: add the 'todolist' plugin

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,22 @@ import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
 // highlightjs
 import hljs from 'highlight.js';
-import createAlignPlugin from '@kangc/v-md-editor/lib/plugins/align';
+VMdEditor.use(githubTheme, {
+  Hljs: hljs,
+});
 
+// align plugin
+import createAlignPlugin from '@kangc/v-md-editor/lib/plugins/align';
+VMdEditor.use(createAlignPlugin());
 
 import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
 
-VMdEditor.use(githubTheme, {
-  Hljs: hljs,
-});
-VMdEditor.use(createAlignPlugin());
-
-VMdPreview.use(githubTheme, {
-  Hljs: hljs,
-});
+// TODO list
+import createTodoListPlugin from '@kangc/v-md-editor/lib/plugins/todo-list/index';
+import '@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css';
+VMdEditor.use(createTodoListPlugin());
 
 const app = createApp(App);
 app.use(VMdEditor);
